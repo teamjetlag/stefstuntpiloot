6c1b7a0d19bffebbc0b2fa2f3940c13584c38463
Carel Coenraad <carel.coenraad@nl.abnamro.com>
2017-02-10 10:31:22 +0100

    Commited

---
+++ .next/3.c43b64b71b4e6a57ce56.hot-update.js
@@ -0,0 +1,48 @@
+webpackHotUpdate(3,{
+
+/***/ 121:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	Object.defineProperty(exports, "__esModule", {
+	  value: true
+	});
+	
+	var _react = __webpack_require__(106);
+	
+	var _react2 = _interopRequireDefault(_react);
+	
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+	
+	exports.default = function () {
+	  return _react2.default.createElement(
+	    "div",
+	    null,
+	    "Welcome to next.js!"
+	  );
+	};
+	//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkQ6XFxQcm9qZWN0c1xcc3RlZnN0dW50cGlsb290XFxwYWdlc1xcaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O2tCQUFlO0FBQUEsU0FDYjtBQUFBO0FBQUE7QUFBQTtBQUFBLEdBRGE7QUFBQSxDIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlUm9vdCI6IkQ6L1Byb2plY3RzL3N0ZWZzdHVudHBpbG9vdCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0ICgpID0+IChcbiAgPGRpdj5XZWxjb21lIHRvIG5leHQuanMhPC9kaXY+XG4pXG4iXX0=
+	    if (true) {
+	      module.hot.accept()
+
+	      var Component = module.exports.default || module.exports
+	      Component.__route = "/"
+
+	      if (module.hot.status() !== 'idle') {
+	        var components = next.router.components
+	        for (var r in components) {
+	          if (!components.hasOwnProperty(r)) continue
+
+	          if (components[r].Component.__route === "/") {
+	            next.router.update(r, Component)
+	          }
+	        }
+	      }
+	    }
+	  
+
+/***/ }
+
+})
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly8vLi9EOi9Qcm9qZWN0cy9zdGVmc3R1bnRwaWxvb3QvcGFnZXMvaW5kZXguanM/Yzg0MyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiOzs7Ozs7Ozs7Ozs7Ozs7OzttQkFBZTtBQUFBLFVBQ2I7QUFBQTtBQUFBO0FBQUE7QUFBQSxJQURhO0FBQUEsRSIsImZpbGUiOiIzLmM0M2I2NGI3MWI0ZTZhNTdjZTU2LmhvdC11cGRhdGUuanMiLCJzb3VyY2VzQ29udGVudCI6WyJleHBvcnQgZGVmYXVsdCAoKSA9PiAoXG4gIDxkaXY+V2VsY29tZSB0byBuZXh0LmpzITwvZGl2PlxuKVxuXG5cblxuLy8gV0VCUEFDSyBGT09URVIgLy9cbi8vIC4vRDovUHJvamVjdHMvc3RlZnN0dW50cGlsb290L3BhZ2VzL2luZGV4LmpzIl0sInNvdXJjZVJvb3QiOiIifQ==
---
+++ .next/dist/pages/index.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _react = require("D:\\Projects\\stefstuntpiloot\\node_modules\\react\\react.js");
+
+var _react2 = _interopRequireDefault(_react);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.default = function () {
+  return _react2.default.createElement(
+    "div",
+    null,
+    "Welcome to next.js!"
+  );
+};
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIkQ6XFxQcm9qZWN0c1xcc3RlZnN0dW50cGlsb290XFxwYWdlc1xcaW5kZXguanMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7O2tCQUFlO0FBQUEsU0FDYjtBQUFBO0FBQUE7QUFBQTtBQUFBLEdBRGE7QUFBQSxDIiwiZmlsZSI6ImluZGV4LmpzIiwic291cmNlUm9vdCI6IkQ6L1Byb2plY3RzL3N0ZWZzdHVudHBpbG9vdCIsInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0ICgpID0+IChcbiAgPGRpdj5XZWxjb21lIHRvIG5leHQuanMhPC9kaXY+XG4pXG4iXX0=
