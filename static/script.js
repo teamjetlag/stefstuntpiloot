8a81e76ec58c04bf05a07a35f3a05120783a9776
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2017-02-11 13:22:18 +0100

    SMS ban aan naar 1337

--- static/script.js
+++ static/script.js
@@ -1,63 +1,40 @@
-var scene = document.querySelector('a-scene');
-
-var boxA = document.querySelector('#boxA');
-var boxB = document.querySelector('#boxB');
-
-console.log(THREE)
-
-AFRAME.registerComponent('slider', {
-  schema: {
-    width: {type: 'number', default: 1},
-    height: {type: 'number', default: 1},
-    depth: {type: 'number', default: 1},
-    color: {type: 'color', default: '#AAA'}
-  },
-
-  init: function() {
-    var data = this.data;
-    var el = this.el;
-    this.geometry = new THREE.BoxBufferGeometry(data.width, data.height, data.depth);
-    this.material = new THREE.MeshStandardMaterial({color: data.color});
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-    el.setObject3D('mesh', this.mesh);
-  },
-
-  update: function (oldData) {
-    var data = this.data;
-    var el = this.el;
-    // If `oldData` is empty, then this means we're in the initialization process.
-    // No need to update.
-    if (Object.keys(oldData).length === 0) { return; }
-    // Geometry-related properties changed. Update the geometry.
-    if (data.width !== oldData.width ||
-      data.height !== oldData.height ||
-      data.depth !== oldData.depth) {
-      el.getObject3D('mesh').geometry = new THREE.BoxBufferGeometry(data.width, data.height,
-        data.depth);
-    }
-    // Material-related properties changed. Update the material.
-    if (data.color !== oldData.color) {
-      el.getObject3D('mesh').material.color = data.color;
-    }
-  },
-
-  remove: function () {
-    this.el.removeObject3D('mesh');
-  }
-});
 
 scene.addEventListener('loaded', function() {
 
+  var cylinder = document.createElement('a-cylinder');
+  cylinder.setAttribute('id', 'boxA');
+  cylinder.setAttribute('color', 'white');
+  cylinder.setAttribute('static-body', '');
+  cylinder.setAttribute('position', '0 0 0');
+  cylinder.setAttribute('width', 0.5);
+  cylinder.setAttribute('height', 1);
+  cylinder.setAttribute('depth', 0.5);
+
+  var bol = document.createElement('a-sphere');
+  bol.setAttribute('id', 'boxB');
+  bol.setAttribute('dynamic-body', 'mass: 50');
+  bol.setAttribute('color', 'red');
+  bol.setAttribute('position', '0 5 0.2');
+  bol.setAttribute('radius', '0.5');
+
+  var koekeloekie =   document.querySelector('a-scene').appendChild(cylinder);
+  console.log(Object.keys(koekeloekie), koekeloekie.components);
+  document.querySelector('a-scene').appendChild(cylinder);
+  document.querySelector('a-scene').appendChild(bol);
+  
+  var scene = document.querySelector('a-scene');
+  var boxA = document.querySelector('#boxA');
+  var boxB = document.querySelector('#boxB');
 
   var bodyA = boxA.body;
   var pivotA = new CANNON.Vec3().copy(bodyA.position); // Vector from posA perpendicular to axisA
 
   var bodyB = boxB.body;
   var pivotB = new CANNON.Vec3().copy(bodyB.position); // Vector from posB perpendicular to axisB
-  bodyB.inertia.set
 
   var pointToPointConstraint = new CANNON.PointToPointConstraint(bodyA, pivotA, bodyB, pivotB);
   bodyB.world.addConstraint(pointToPointConstraint);
+
 
 });
 
@@ -70,5 +47,5 @@ setInterval(function() {
   var x = boxB.getAttribute('position').x;
   var z = boxB.getAttribute('position').z;
   var f = 10;
-  boxB.body.applyImpulse(new CANNON.Vec3(z*f, 0, -x*f), new CANNON.Vec3().copy(boxB.getAttribute('position')));
+  boxB.body.applyImpulse(new CANNON.Vec3(z * f, 0, -x * f), new CANNON.Vec3().copy(boxB.getAttribute('position')));
 }, 1500)
