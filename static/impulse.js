b3f3e7688e9d1194244bc2157dadd30dab879a9c
Carel Coenraad <carel.coenraad@nl.abnamro.com>
2017-02-11 12:32:37 +0100

    Adds Chris stuff to next

--- static/impulse.js
+++ static/impulse.js
@@ -1,4 +1,15 @@
 export default function applyImpulse(element) {
-  console.log('Applying impulse');
-  element.body.applyImpulse(new CANNON.Vec3(0, 150, 0), new CANNON.Vec3().copy(element.getAttribute('position')));
+  console.log(element.getAttribute('position').x);
+  console.log(element.getAttribute('position').z);
+  const posx = element.getAttribute('position').x;
+  const posz = element.getAttribute('position').z;
+  if (
+    (posx < -4 && (posz > -1.5 && posz <0)) ||
+    ((posx > -1.5 && posx < 0) && posz > 4) ||
+    (posx > 4 && (posz < 1.5 && posz > 0)) ||
+    ((posx < 1.5 && posx > 0) && posz < -4)
+  ) {
+    console.log('Applying impulse');
+    element.body.applyImpulse(new CANNON.Vec3(0, 150, 0), new CANNON.Vec3().copy(element.getAttribute('position')));
+  }
 }
